af846a47c12cf7c1b4008cf75e6bc076dea12c27
miros777 <syniavskyi1984@gmail.com>
2024-05-02 10:22:24 +0300

    final change main.js

--- lesson_2/scripts/main.js
+++ lesson_2/scripts/main.js
@@ -248,14 +248,59 @@ console.log(nameDay);
 Потрібно знайти та вивести максимальне число з тих двох .
 Також потрібно врахувати коли введені рівні числа.
 */
-let number1 = 100;
+let number1 = 500;
 let number2 = 100;
 
-/*
+if ((typeof number1 && typeof number2) === 'number') {
+    if (number1 > number2) {
+        console.log('Max number:', number1);
+    } else if (number1 === number2) {
+        console.log('Числа рівні, MAX:', number1);
+    } else {
+        console.log('Max number:', number2);
+    }
+}
 
+/*
 - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
  буде присвоювати змінній х значення "default"  якщо ви намагаєтесь присвоїти в неї falsy-значення (хибноподібні, тобто ті, які приводиться до false,
  а це 0 null undefined і тд).
+ */
 
+let xxx = 'string';
+if(xxx === 0 || xxx === undefined || xxx === null || xxx === false || xxx === ''){
+    xxx = 'default';
+}
+console.log(xxx);
 
-- з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання. У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".*/
+/*
+- з файлу arrays.js (лежить в папці 2023 plan ) взяти масив coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на тривалість навчання.
+У випадку якщо тривалість довша за 5 місяців вивести в консоль "Супер".
+*/
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+if(coursesAndDurationArray[0].monthDuration > 5){
+    console.log(coursesAndDurationArray[0].title + ':', 'Супер');
+}
+if(coursesAndDurationArray[1].monthDuration > 5){
+    console.log(coursesAndDurationArray[1].title + ':', 'Супер');
+}
+if(coursesAndDurationArray[2].monthDuration > 5){
+    console.log(coursesAndDurationArray[2].title + ':', 'Супер');
+}
+if(coursesAndDurationArray[3].monthDuration > 5){
+    console.log(coursesAndDurationArray[3].title + ':', 'Супер');
+}
+if(coursesAndDurationArray[4].monthDuration > 5){
+    console.log(coursesAndDurationArray[4].title + ':', 'Супер');
+}
+if(coursesAndDurationArray[5].monthDuration > 5){
+    console.log(coursesAndDurationArray[5].title + ':', 'Супер');
+}
