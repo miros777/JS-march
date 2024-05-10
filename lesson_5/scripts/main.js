4dede1213d942522d7de4b73af9c8c056ea41b66
miros777 <syniavskyi1984@gmail.com>
2024-05-10 14:00:04 +0300

    change main.js lesson 5

--- lesson_5/scripts/main.js
+++ lesson_5/scripts/main.js
@@ -132,3 +132,11 @@ console.log(swap([11,22,33,44],0,1));
 - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
 */
+let exchange = (sumUAH,currencyValues,exchangeCurrency) => {
+    for (const obj of currencyValues) {
+        if(obj.currency === exchangeCurrency){
+            return sumUAH/obj.value;
+        }
+    }
+}
+console.log(exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD'))
