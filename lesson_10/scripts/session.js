32c5b78c6ad394e2fabac26fb6901d53b7aaebb2
miros777 <syniavskyi1984@gmail.com>
2024-05-26 13:05:16 +0300

    change and add session files lesson 10

---
+++ lesson_10/scripts/session.js
@@ -0,0 +1,49 @@
+/*
+==========================
+Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessions зберігається інформація
+про дату та час відвідування сторінки. Є ще сторінка sessions.html (назва довільна), при відвідуванні якої потрібно відмалювати всю
+інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль, а побудувати дом структуру під кожну сессію
+=========================
+*/
+function getNamePageNow(path = window.location.pathname, namePage) {
+    let slicePath = path.split('/');
+    let checkPage = slicePath.find(el => {
+        if (el === namePage) {
+            return el;
+        }
+    });
+    return checkPage;
+}
+
+if (getNamePageNow(window.location.pathname, 'index.html') === 'index.html') {
+    let newDate = new Date();
+    let obj = {date: newDate.getDate(), month: newDate.getMonth(), year: newDate.getFullYear(), hours: newDate.getHours(), min: newDate.getMinutes()};
+
+    if (!localStorage.getItem('sessions')) {
+        let arr = [];
+        arr.push(obj);
+        localStorage.setItem('sessions', JSON.stringify(arr));
+    } else {
+        let pars = JSON.parse(localStorage.getItem('sessions'));
+        pars.push(obj);
+        localStorage.setItem('sessions', JSON.stringify(pars));
+    }
+}
+
+if (getNamePageNow(window.location.pathname, 'session.html') === 'session.html') {
+    let res = localStorage.getItem('sessions')
+    let resObj = JSON.parse(res);
+
+    let ul = document.createElement('ul');
+    for (const OneObj of resObj) {
+        res = '';
+        for (const oneObjKey in OneObj) {
+            res += `${oneObjKey}: ${OneObj[oneObjKey]} `;
+        }
+        let li = document.createElement('li');
+        li.innerText = 'Session - ' + res;
+        ul.appendChild(li);
+
+        document.body.appendChild(ul);
+    }
+}
