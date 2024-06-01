ad332baf58d2978342d12e1b29ae34dce888bb89
miros777 <syniavskyi1984@gmail.com>
2024-06-02 01:26:06 +0300

    add changes for min project

--- mini-project JS/scripts/main.js
+++ mini-project JS/scripts/main.js
@@ -3,8 +3,9 @@
 2 Вивести id,name всіх user в index.html. Окремий блок для кожного user.
 3 Додати кожному блоку кнопку/посилання , при кліку на яку відбувається перехід  на сторінку user-details.html, котра має детальну інфорацію про об'єкт на який клікнули
 */
+
 async function getDataAllUsersFromAPI() {
-    const users = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const usersAll = await fetch(`https://jsonplaceholder.typicode.com/users`)
         .then(res => res.json())
         .then((users) => {
 
@@ -13,6 +14,9 @@ async function getDataAllUsersFromAPI() {
             for (const val in users) {
                 let divUser = document.createElement('div');
                 divUser.setAttribute('class', 'user');
+
+                let divContent = document.createElement('div');
+                divContent.setAttribute('class', 'userContent');
 
                 let spanID = document.createElement('span');
                 spanID.setAttribute('class', 'userID');
@@ -32,7 +36,9 @@ async function getDataAllUsersFromAPI() {
                 linkUser.innerText = name;
                 btnUser.appendChild(linkUser);
 
-                divUser.append(spanID, spanName, btnUser);
+                divContent.append(spanID, spanName);
+
+                divUser.append(divContent, btnUser);
                 usersDiv.appendChild(divUser);
             }
 
@@ -46,12 +52,182 @@ async function getDataAllUsersFromAPI() {
 (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
 6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
 */
+
 async function getDataUserFromAPI() {
-    const users = await fetch(`https://jsonplaceholder.typicode.com/users/`)
+
+    let params = new URLSearchParams(document.location.search);
+    let idUser = params.get("id");
+
+    const users = await fetch(`https://jsonplaceholder.typicode.com/users/${idUser}`)
         .then(res => res.json())
-        .then((users) => {
+        .then((user) => {
+
+            let divUser = document.createElement('div');
+            divUser.setAttribute('class', 'user');
+
+            let divContent = document.createElement('div');
+            divContent.setAttribute('class', 'userContent');
+
+            let spanID = document.createElement('span');
+            spanID.setAttribute('class', 'userID userValue');
+
+            let spanName = document.createElement('span');
+            spanName.setAttribute('class', 'userName userValue');
+
+            let spanUsername = document.createElement('span');
+            spanUsername.setAttribute('class', 'userSurname userValue');
+
+            let spanEmail = document.createElement('span');
+            spanEmail.setAttribute('class', 'userEmail userValue');
+
+            let spanPhone = document.createElement('span');
+            spanPhone.setAttribute('class', 'userPhone userValue');
+
+            let spanWebsite = document.createElement('span');
+            spanWebsite.setAttribute('class', 'userName userValue');
+
+            // address
+            let spanCity = document.createElement('span');
+            spanCity.setAttribute('class', 'userCity userValue');
+
+            let spanGeoLat = document.createElement('span');
+            spanGeoLat.setAttribute('class', 'userGroLat userValue');
+
+            let spanGeoLng = document.createElement('span');
+            spanGeoLng.setAttribute('class', 'userGroLng userValue');
+
+            let spanStreet = document.createElement('span');
+            spanStreet.setAttribute('class', 'userStreet userValue');
+
+            let spanSuite = document.createElement('span');
+            spanSuite.setAttribute('class', 'userSuite userValue');
+
+            let spanZipcode = document.createElement('span');
+            spanZipcode.setAttribute('class', 'userZip userValue');
+
+            // company
+            let spanBs = document.createElement('span');
+            spanBs.setAttribute('class', 'userBs userValue');
+
+            let spanCatch = document.createElement('span');
+            spanCatch.setAttribute('class', 'userCatch userValue');
+
+            let spanNameCompany = document.createElement('span');
+            spanNameCompany.setAttribute('class', 'userNameCompany userValue');
+
+            let {id, name, username, email, phone, website, address, company} = user;
+
+            spanID.innerText = `id: ${id}`;
+            spanName.innerText = name;
+            spanUsername.innerText = username;
+            spanEmail.innerText = email;
+            spanPhone.innerText = phone;
+            spanWebsite.innerText = website;
+
+            spanCity.innerText = address.city;
+            spanGeoLat.innerText = address.geo.lat;
+            spanGeoLng.innerText = address.geo.lng;
+            spanStreet.innerText = address.street;
+            spanSuite.innerText = address.suite;
+            spanZipcode.innerText = address.zipcode;
+
+            spanBs.innerText = company.bs;
+            spanCatch.innerText = company.catchPhrase;
+            spanNameCompany.innerText = company.name;
+
+            let btnUserPosts = document.createElement('button');
+            btnUserPosts.setAttribute('id', 'allPosts');
+            let linkUser = document.createElement('a');
+            linkUser.innerText = 'post of current user';
+
+            btnUserPosts.appendChild(linkUser);
+
+            divContent.append(spanID, spanName, spanUsername, spanEmail, spanPhone, spanWebsite, spanCity, spanGeoLat, spanGeoLng, spanStreet, spanSuite, spanZipcode, spanBs, spanCatch, spanNameCompany);
+
+            divUser.append(divContent, btnUserPosts);
+            document.body.appendChild(divUser);
+
+            // console.log(user);
+
+            btnUserPosts.addEventListener('click', function (e) {
+                fetch(`https://jsonplaceholder.typicode.com/users/${idUser}/posts`)
+                    .then(value => value.json())
+                    .then(posts => {
+                        let getUserDiv = document.getElementsByClassName('user')[0];
+                        let divPostAll = document.createElement('div');
+                        divPostAll.setAttribute('class', 'postAll');
+
+                        for (const post of posts) {
+
+                            let divPost = document.createElement('div');
+                            divPost.setAttribute('class', 'postOne');
+
+                            let divPostTitle = document.createElement('div');
+                            divPostTitle.setAttribute('class', 'postTitle');
+                            divPostTitle.innerText = `${post.title}`;
+
+                            let divPostBtn = document.createElement('button');
+                            divPostBtn.setAttribute('id', `post_${post.id}`);
+                            let postLink = document.createElement('a');
+                            postLink.href = `http://localhost:63342/JS-march/mini-project%20JS/post-details.html?id=${post.id}&idUser=${idUser}`;
+                            postLink.innerText = 'Read more...';
+                            divPostBtn.appendChild(postLink);
+
+                            divPost.append(divPostTitle, divPostBtn);
+
+                            divPostAll.appendChild(divPost);
+                            getUserDiv.appendChild(divPostAll);
+                        }
+
+                        //
+
+                        // return idUser;
+                    })
 
             });
+
+        });
+}
+
+async function getUserPostsFromAPI() {
+    let params = new URLSearchParams(document.location.search);
+    let idUser = params.get("idUser");
+    let idPost = params.get("id");
+
+    let post = await fetch(`https://jsonplaceholder.typicode.com/users/${idUser}`)
+
+        .then(val => val.json())
+        .then(user => {
+            return user;
+        }).then(users => {
+                return fetch(`https://jsonplaceholder.typicode.com/users/${idUser}/posts`);
+            }
+        )
+        .then(val => val.json())
+        .then(posts => {
+            for (const post of posts) {
+                if (post.id == idPost) {
+                    // console.log(post);
+                    console.log(post.id);
+                    console.log(post.title);
+                    console.log(post.userId);
+                    console.log(post.body);
+                }
+
+            }
+            return idPost;
+        })
+        .then(posts => {
+            return fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}/comments`);
+        })
+        .then(val => val.json())
+        .then(comments => {
+            console.log(comments)
+        })
+    // console.log(post)
+
+
+    // return post;
 }
 
 /*
